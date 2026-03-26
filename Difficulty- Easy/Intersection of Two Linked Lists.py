f5d36088f033d96563b1e5c0038fbd306aa95aaf
Kunal Kumar <kunalsinghrajput9142@gmail.com>
2026-03-26 22:22:55 +0530

    Add files via upload

---
+++ Difficulty- Easy/Intersection of Two Linked Lists.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        if not headA or not headB:
+            return None
+        
+        p1, p2 = headA, headB
+        
+        while p1 != p2:
+            p1 = p1.next if p1 else headB
+            p2 = p2.next if p2 else headA
+        
+        return p1
+        
